660ada0026469b2131edc705a02516c9f2718f0c
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2021-05-25 17:26:56 +0530

    status code updates

--- src/main/java/com/springboot/practice/controller/MyController.java
+++ src/main/java/com/springboot/practice/controller/MyController.java
@@ -1,8 +1,11 @@
 package com.springboot.practice.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,31 +31,65 @@ public class MyController {
 	
 	
 	@GetMapping("/book")
-	public List<Book> getBooks()
+	public ResponseEntity<List<Book>> getBooks()
 	{
-		return bookService.getAllBook();
+		List<Book> book = bookService.getAllBook();
+		if(book.size() <= 0)
+		{
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(book));
 	}
 	
 	@GetMapping("/book/{id}")
-	public Book getBook(@PathVariable("id") int id)
+	public ResponseEntity<Book> getBook(@PathVariable("id") int id)
 	{
-		return bookService.getBookById(id);
+		Book book =  bookService.getBookById(id);
+		
+		if(book == null)
+		{
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(book));
 	}
 	@PostMapping("/books")
-	public void addBooks(@RequestBody Book book)
+	public ResponseEntity<Book> addBooks(@RequestBody Book book)
 	{
-		bookService.addBook(book);
+		Book bk = null;
+		try {
+		bk = bookService.addBook(book);
+		return ResponseEntity.of(Optional.of(bk));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	
 	@DeleteMapping("/book/{id}")
-	public void deleteBook(@PathVariable("id") int id)
+	public ResponseEntity<Void> deleteBook(@PathVariable("id") int id)
 	{
+		try {
 		bookService.deleteBook(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 	
 	@PutMapping("/book/{id}")
-	public void updateBook(@RequestBody Book book, @PathVariable("id") int id)
+	public ResponseEntity<Book> updateBook(@RequestBody Book book, @PathVariable("id") int id)
 	{
+		try {
 		bookService.updateBook(book, id);
+		return ResponseEntity.ok().body(book);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 }
--- src/main/java/com/springboot/practice/service/BookService.java
+++ src/main/java/com/springboot/practice/service/BookService.java
@@ -7,52 +7,54 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 import com.springboot.practice.entities.Book;
+
 @Component
 public class BookService {
-	
+
 	private static List<Book> list = new ArrayList<>();
-	
+
 	static {
-		list.add(new Book(321, "Head First java","Kathy Sierra", 470));
-		list.add(new Book(121, "Introduction to Algorithms","Thomas H. Cormen", 1500));
-		list.add(new Book(325, "Head First Design Patterns","Kathy Sierra", 260));
-		list.add(new Book(333, "Clean Code","Robert Cecil Martin", 500));
-		list.add(new Book(222, "The Algorithm Design Manual","Steven Skiena", 450));
-		list.add(new Book(111, "Big Data Analytics with Hadoop","Sridhar Alla",680));
+
+		list.add(new Book(321, "Head First java", "Kathy Sierra", 470));
+		list.add(new Book(121, "Introduction to Algorithms", "Thomas H. Cormen", 1500));
+		list.add(new Book(325, "Head First Design Patterns", "Kathy Sierra", 260));
+		list.add(new Book(333, "Clean Code", "Robert Cecil Martin", 500));
+		list.add(new Book(222, "The Algorithm Design Manual", "Steven Skiena", 450));
+		list.add(new Book(111, "Big Data Analytics with Hadoop", "Sridhar Alla", 680));
+
 	}
-	
-	public List<Book> getAllBook()
-	{
+
+	public List<Book> getAllBook() {
 		return list;
 	}
-	
-	public Book getBookById(int id)
-	{
+
+	public Book getBookById(int id) {
 		return list.stream().filter(b -> b.getId() == id).findFirst().get();
 	}
 
-	public void addBook(Book book) {
+	public Book addBook(Book book) {
 		list.add(book);
+		return book;
 	}
 
 	public void deleteBook(int id) {
-		
+
 		list = list.stream().filter(book -> book.getId() != id).collect(Collectors.toList());
-		
+
 	}
 
-	public void updateBook(Book book, int id) {
-		
-		list = list.stream().map(b ->{
-			if(b.getId() == id)
-			{
+	public Book updateBook(Book book, int id) {
+
+		list = list.stream().map(b -> {
+			if (b.getId() == id) {
 				b.setTitle(book.getTitle());
 				b.setAuthor(book.getAuthor());
 				b.setPages(book.getPages());
 			}
 			return b;
 		}).collect(Collectors.toList());
-		
+
+		return book;
 	}
 
 }
